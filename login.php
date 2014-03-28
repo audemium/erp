66efe43e721425b1fd0646438966d09cb81e04ad
Nicholas Anderson <nicholas.anderson2010@gmail.com>
2014-03-28 14:56:36 -0400

    Moved login.js into login.php.

--- login.php
+++ login.php
@@ -22,7 +22,56 @@
 		require('head.php');
 	?>
 	<link type="text/css" rel="stylesheet" href="css/login.css">
-	<script type="text/javascript" src="js/login.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('input').focusout(function() {
+				if ($(this).val() == '') {
+					$(this).addClass('invalid');
+					$(this).next().css('visibility', 'visible');
+				}
+				else {
+					$(this).removeClass('invalid');
+					$(this).next().css('visibility', 'hidden');
+				}
+			});
+
+			$('#loginForm').submit(function(event) {
+				//password is first so username gets focus if both are empty
+				if ($('#password').val() == '') {
+					$('#password').addClass('invalid');
+					$('#passwordError').show();
+					$('#password').focus();
+				}
+				if ($('#username').val() == '') {
+					$('#username').addClass('invalid');
+					$('#usernameError').show();
+					$('#username').focus();
+				}
+				
+				if ($('#username').val() != '' && $('#password').val() != '') {
+					$.ajax({
+						url: 'ajax.php',
+						type: 'POST',
+						data: {
+							'action': 'login',
+							'username': $('#username').val(),
+							'password': $('#password').val()
+						}
+					}).done(function(data) {
+						if (data.status == 'success') {
+							window.location.replace(data.redirect);
+						}
+						else {
+							$('#username').val('');
+							$('#password').val('');
+							$('#loginError').css('visibility', 'visible');
+						}
+					});
+				}
+				event.preventDefault();
+			});
+		});
+	</script>
 </head>
 
 <body>
