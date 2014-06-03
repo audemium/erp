1cffd8f416d1e885fbe747fa75085cd61c9f3a4f
Nicholas Anderson <nicholas.anderson2010@gmail.com>
2014-06-03 17:29:17 -0400

    Changed array declaration to brackets.

--- ajax.php
+++ ajax.php
@@ -20,13 +20,13 @@
 		Outputs: status (success / fail), redirect
 	*/
 	if ($_POST['action'] == 'login') {
-		$return = array('status' => 'fail');
+		$return = ['status' => 'fail'];
 		
 		$sth = $dbh->prepare(
 			'SELECT employeeID, password, timeZone
 			FROM employees
 			WHERE username = :username');
-		$sth->execute(array(':username' => $_POST['username']));
+		$sth->execute([':username' => $_POST['username']]);
 		$result = $sth->fetchAll();
 		if (count($result) == 1) {
 			if (password_verify($_POST['password'], $result[0]['password'])) {
@@ -37,7 +37,7 @@
 				$_SESSION['timeZone'] = $result[0]['timeZone'];
 				$redirect = (isset($_SESSION['loginDestination']) && $_SESSION['loginDestination'] != '/login.php') ? $_SESSION['loginDestination'] : '/index.php';
 				unset($return);
-				$return = array('status' => 'success', 'redirect' => $redirect);
+				$return = ['status' => 'success', 'redirect' => $redirect];
 				unset($_SESSION['loginDestination']);
 			}
 		}
@@ -51,16 +51,16 @@
 		Outputs: status (success / fail), results
 	*/
 	if ($_POST['action'] == 'search') {
-		$return = array('status' => 'fail');
+		$return = ['status' => 'fail'];
 		
 		//TODO: get search results
 		unset($return);
-		$results = array(
-			array('name' => 'Nicholas Anderson', 'type' => 'employeee', 'image' => 'user_blue_32.png', 'url' => 'employees.php?id=1'),
-			array('name' => 'Website Development', 'type' => 'service', 'image' => 'basket_32.png', 'url' => 'services.php?id=1'),
-			array('name' => 'Owner', 'type' => 'position', 'image' => 'user_business_32.png', 'url' => 'positions.php?id=1')
-		);
-		$return = array('status' => 'success', 'results' => $results);
+		$results = [
+			['name' => 'Nicholas Anderson', 'type' => 'employeee', 'image' => 'user_blue_32.png', 'url' => 'employees.php?id=1'],
+			['name' => 'Website Development', 'type' => 'service', 'image' => 'basket_32.png', 'url' => 'services.php?id=1'],
+			['name' => 'Owner', 'type' => 'position', 'image' => 'user_business_32.png', 'url' => 'positions.php?id=1']
+		];
+		$return = ['status' => 'success', 'results' => $results];
 		
 		echo json_encode($return);
 	}
@@ -71,7 +71,7 @@
 		Outputs: status (success / fail), html
 	*/
 	if ($_POST['action'] == 'add') {
-		$return = array('status' => 'success');
+		$return = ['status' => 'success'];
 		
 		//get list of locations
 		$locationOptions = '';
@@ -98,19 +98,18 @@
 		}
 		
 		$return['html'] = 
-			'<a id="close" title="Close">X</a>
-			<h1>Add employee</h1>
+			'<h1>Add employee</h1>
 			<section>
 				<h2>Basic Information</h2>
 				<div class="sectionData">
 					<ul>
 						<li>
 							<label for="firstName">First Name</label>
-							<input type="text" name="firstName">
+							<input type="text" name="firstName" autocomplete="off">
 						</li>
 						<li>
 							<label for="lastName">Last Name</label>
-							<input type="text" name="lastName">
+							<input type="text" name="lastName" autocomplete="off">
 						</li>
 						<li>
 							<label for="payType">Pay Type</label>
@@ -122,7 +121,7 @@
 						</li>
 						<li>
 							<label for="payAmount">Pay Amount</label>
-							<input type="text" name="payAmount">
+							<input type="text" name="payAmount" autocomplete="off">
 						</li>
 					</ul>
 					<ul>
@@ -149,7 +148,7 @@
 					<ul>
 						<li>
 							<label for="address">Address</label>
-							<input type="text" name="address">
+							<input type="text" name="address" autocomplete="off">
 						</li>
 					</ul>
 				</div>
@@ -160,19 +159,95 @@
 	}
 	
 	/*
+		Function: addSave
+		Inputs: 
+		Outputs: 
+	*/
+	if ($_POST['action'] == 'addSave') {
+		//verify data
+		foreach ($_POST as $key => $value) {
+			if ($key != 'action' && $key != 'type') {
+				$data[$key] = $value;
+			}
+		}
+		$return = verifyData($_POST['type'], $data);
+		
+		if ($return['status'] != 'fail') {
+			foreach ($data as $key => $value) {
+				$keyArr[] = $key;
+				$placeholderArr[] = '?';
+				$valArr[] = $value;
+				$changeData[$key] = $value;
+			}
+			$tableName = $TYPES[$_POST['type']]['pluralName'];
+			$idName = $TYPES[$_POST['type']]['idName'];
+			
+			if ($_POST['type'] == 'employee') {
+				//generate username
+				$alphaUser = strtolower(substr($_POST['firstName'], 0, 1).substr($_POST['lastName'], 0, 1));
+				$sth = $dbh->prepare(
+					'SELECT MAX(CAST(SUBSTRING(username, 3, LENGTH(username) - 2) AS UNSIGNED)) AS maxID
+					FROM employees
+					WHERE username LIKE '.$dbh->quote($alphaUser.'%'));
+				$sth->execute();
+				$row = $sth->fetch();
+				$idNum = (is_null($row['maxID'])) ? 1 : $row['maxID'] + 1;
+				$keyArr[] = 'username';
+				$placeholderArr[] = '?';
+				$valArr[] = $alphaUser.$idNum;
+				
+				//generate temp password (excludes 0, o, 1, l and vowels)
+				$characters = 'bcdfghjkmnpqrstvwxyz23456789';
+				$length = strlen($characters) - 1;
+				$password = '';
+				for ($i = 0; $i < 8; $i++) {
+					$password .= $characters[mt_rand(0, $length)];
+				}
+				$hash = password_hash($password, PASSWORD_BCRYPT, ['cost' => 10]);
+				$keyArr[] = 'password';
+				$placeholderArr[] = '?';
+				$valArr[] = $hash;
+			}
+			
+			//add to changes table
+			addChange($_POST['type'], $id, $_SESSION['employeeID'], json_encode($changeData));
+			
+			//do the insert
+			$sth = $dbh->prepare(
+				'INSERT INTO '.$tableName.' ('.$idName.', active, '.implode(', ', $keyArr).')
+				VALUES(null, 1, '.implode(', ', $placeholderArr).')');
+			$sth->execute($valArr);
+			$id = $dbh->lastInsertId();
+			
+			if ($_POST['type'] == 'employee') {
+				$return['html'] = 
+					'<h1>Add employee</h1>
+					<div style="text-align: center;"><br>
+						Employee <a href="employees_item.php?id='.$id.'">'.$_POST['firstName'].' '.$_POST['lastName'].'</a> was added.<br>
+						Save the username and temporary password below to give to the employee.<br><br>
+						<b>Username: </b>'.$alphaUser.$idNum.'<br>
+						<b>Temporary Password: </b>'.$password.'
+					</div>';
+			}
+		}
+		
+		echo json_encode($return);
+	}
+	
+	/*
 		Function: edit
 		Inputs: id
 		Outputs: status (success / fail), html
 	*/
 	if ($_POST['action'] == 'edit') {
-		$return = array('status' => 'success');
+		$return = ['status' => 'success'];
 		
 		//get current values
 		$sth = $dbh->prepare(
 			'SELECT username, firstName, lastName, payType, payAmount, locationID, positionID, active
 			FROM employees
 			WHERE employeeID = :employeeID');
-		$sth->execute(array(':employeeID' => $_POST['id']));
+		$sth->execute([':employeeID' => $_POST['id']]);
 		$employee = $sth->fetch();
 		
 		//get list of locations
@@ -215,41 +290,40 @@
 		
 		
 		$return['html'] = 
-			'<a id="close" title="Close">X</a>
-			<h1>Edit employee</h1>
+			'<h1>Edit employee</h1>
 			<section>
 				<h2>Basic Information</h2>
 				<div class="sectionData">
 					<ul>
 						<li>
 							<label for="firstName">First Name</label>
-							<input type="text" id="firstName" value="'.$employee['firstName'].'">
+							<input type="text" name="firstName" autocomplete="off" value="'.$employee['firstName'].'">
 						</li>
 						<li>
 							<label for="lastName">Last Name</label>
-							<input type="text" id="lastName" value="'.$employee['lastName'].'">
+							<input type="text" name="lastName" autocomplete="off" value="'.$employee['lastName'].'">
 						</li>
 						<li>
 							<label for="payType">Pay Type</label>
-							<select id="payType">
+							<select name="payType">
 								'.$payTypeOptions.'
 							</select>
 						</li>
 						<li>
 							<label for="payAmount">Pay Amount</label>
-							<input type="text" id="payAmount" value="'.$employee['payAmount'].'">
+							<input type="text" name="payAmount" autocomplete="off" value="'.$employee['payAmount'].'">
 						</li>
 					</ul>
 					<ul>
 						<li>
 							<label for="locationID">Location</label>
-							<select id="locationID">
+							<select name="locationID">
 								'.$locationOptions.'
 							</select>
 						</li>
 						<li>
 							<label for="positionID">Position</label>
-							<select id="positionID">
+							<select name="positionID">
 								'.$positionOptions.'
 							</select>
 						</li>
@@ -262,7 +336,7 @@
 					<ul>
 						<li>
 							<label for="address">Address</label>
-							<input type="text" id="address">
+							<input type="text" name="address" autocomplete="off">
 						</li>
 					</ul>
 				</div>
@@ -278,7 +352,7 @@
 		Outputs: status (success / fail), html
 	*/
 	if ($_POST['action'] == 'editMany') {
-		$return = array('status' => 'success');
+		$return = ['status' => 'success'];
 		
 		//get list of locations
 		$locationOptions = '';
@@ -305,8 +379,7 @@
 		}
 		
 		$return['html'] = 
-			'<a id="close" title="Close">X</a>
-			<h1>Edit employees</h1>
+			'<h1>Edit employees</h1>
 			<section>
 				<h2>Basic Information</h2>
 				<div class="sectionData">
@@ -314,17 +387,17 @@
 						<li>
 							<input type="checkbox">
 							<label for="firstName">First Name</label>
-							<input type="text" id="firstName" disabled>
+							<input type="text" name="firstName" autocomplete="off" disabled>
 						</li>
 						<li>
 							<input type="checkbox">
 							<label for="lastName">Last Name</label>
-							<input type="text" id="lastName" disabled>
+							<input type="text" name="lastName" autocomplete="off" disabled>
 						</li>
 						<li>
 							<input type="checkbox">
 							<label for="payType">Pay Type</label>
-							<select id="payType" disabled>
+							<select name="payType" disabled>
 								<option value=""></option>
 								<option value="H">Hourly</option>
 								<option value="S">Salary</option>
@@ -333,21 +406,21 @@
 						<li>
 							<input type="checkbox">
 							<label for="payAmount">Pay Amount</label>
-							<input type="text" id="payAmount" disabled>
+							<input type="text" name="payAmount" autocomplete="off" disabled>
 						</li>
 					</ul>
 					<ul>
 						<li>
 							<input type="checkbox">
 							<label for="locationID">Location</label>
-							<select id="locationID" disabled>
+							<select name="locationID" disabled>
 								'.$locationOptions.'
 							</select>
 						</li>
 						<li>
 							<input type="checkbox">
 							<label for="positionID">Position</label>
-							<select id="positionID" disabled>
+							<select name="positionID" disabled>
 								'.$positionOptions.'
 							</select>
 						</li>
@@ -361,7 +434,7 @@
 						<li>
 							<input type="checkbox">
 							<label for="address">Address</label>
-							<input type="text" id="address" disabled>
+							<input type="text" name="address" autocomplete="off" disabled>
 						</li>
 					</ul>
 				</div>
@@ -372,19 +445,119 @@
 	}
 	
 	/*
+		Function: editSave
+		Inputs: 
+		Outputs: 
+	*/
+	if ($_POST['action'] == 'editSave') {
+		//verify data
+		foreach ($_POST as $key => $value) {
+			if ($key != 'action' && $key != 'type' && $key != 'id') {
+				$data[$key] = $value;
+			}
+		}
+		$return = verifyData($_POST['type'], $data);
+		
+		if ($return['status'] != 'fail') {
+			//make ids safe
+			$idArr = explode(',', $_POST['id']);
+			foreach ($idArr as $id) {
+				$idArrSafe[] = $dbh->quote($id);
+			}
+			$ids = implode(',', $idArrSafe);
+			
+			foreach ($data as $key => $value) {
+				$updateArr[] = $key.' = ?';
+				$valArr[] = $value;
+				$changeData[$key] = $value;
+			}
+			$updateStr = implode(', ', $updateArr);
+			$tableName = $TYPES[$_POST['type']]['pluralName'];
+			$idName = $TYPES[$_POST['type']]['idName'];
+			
+			//add to changes table, include only what actually changed for each item
+			foreach ($idArr as $id) {
+				$sth = $dbh->prepare(
+					'SELECT * FROM '.$tableName.'
+					WHERE '.$idName.' = :id');
+				$sth->execute([':id' => $id]);
+				$row = $sth->fetch();
+				$tempData = [];
+				foreach ($changeData as $key => $value) {
+					if ($row[$key] != $value) {
+						$tempData[$key] = $value;
+					}
+				}
+				if (count($tempData) > 0) {
+					addChange($_POST['type'], $id, $_SESSION['employeeID'], json_encode($tempData));
+				}
+			}
+			
+			//do the update
+			$sth = $dbh->prepare(
+				'UPDATE '.$tableName.'
+				SET '.$updateStr.'
+				WHERE '.$idName.' IN ('.$ids.')');
+			$sth->execute($valArr);
+		}
+		
+		echo json_encode($return);
+	}
+	
+	/*
 		Function: delete
 		Inputs: 
 		Outputs: status (success / fail), html
 	*/
 	if ($_POST['action'] == 'delete') {
-		$return = array('status' => 'success');
+		$return = ['status' => 'success'];
 		
 		$return['html'] = 
-			'<a id="close" title="Close">X</a>
-			<h1>Delete employee</h1><br>
-			<div style="text-align: center;">Are you sure you want to delete this employee?  This will mark the employee as inactive, but will retain their information.</div>
+			'<h1>Delete '.$_POST['type'].'</h1><br>
+			<div style="text-align: center;">Are you sure you want to delete <b>'.getName($_POST['type'], $_POST['id']).'</b>?  This will mark the '.$_POST['type'].' as inactive, but will retain historical information.</div>
 			<div id="btnSpacer"><button id="deleteBtn">Delete</button></div>';
 		
 		echo json_encode($return);
+	}
+	
+	/*
+		Function: deleteMany
+		Inputs: 
+		Outputs: status (success / fail), html
+	*/
+	if ($_POST['action'] == 'deleteMany') {
+		$return = ['status' => 'success'];
+		$pluralName = $TYPES[$_POST['type']]['pluralName'];
+		
+		$return['html'] = 
+			'<h1>Delete '.$pluralName.'</h1><br>
+			<div style="text-align: center;">Are you sure you want to delete these '.$pluralName.'?  This will mark the '.$pluralName.' as inactive, but will retain historical information.</div>
+			<div id="btnSpacer"><button id="deleteBtn">Delete</button></div>';
+		
+		echo json_encode($return);
+	}
+	
+	/*
+		Function: deleteSave
+		Inputs: 
+		Outputs: 
+	*/
+	if ($_POST['action'] == 'deleteSave') {
+		$return = ['status' => 'success'];
+		
+		//make ids safe
+		$idArr = explode(',', $_POST['id']);
+		foreach ($idArr as $id) {
+			$idArrSafe[] = $dbh->quote($id);
+		}
+		$ids = implode(',', $idArrSafe);
+		$tableName = $TYPES[$_POST['type']]['pluralName'];
+		$idName = $TYPES[$_POST['type']]['idName'];
+		
+		//do the delete
+		$sth = $dbh->prepare(
+			'DELETE FROM '.$tableName.'
+			WHERE '.$idName.' IN ('.$ids.')');
+		$sth->execute($valArr);
 	}
 ?>
