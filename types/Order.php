a94528d67e0089c66e9c93cb7d30c42fc0b96033
Nicholas Anderson <nicholas.anderson2010@gmail.com>
2014-07-29 12:52:59 -0400

    Moved invoice code to Order.php, although i didn't test if it works as an email.

--- types/Order.php
+++ types/Order.php
@@ -15,8 +15,117 @@
 		public function printItemBody($id) {
 			global $dbh;
 			global $TYPES;
+			$subTotal = 0;
 		
-			$return = 'stuff will go here';
+			$return = '<section>
+				<h2>Line Items</h2>
+				<div class="sectionData">
+					<table class="orderTable" style="width:100%;">
+						<thead style="font-weight:bold;">
+							<tr>
+								<th>Item</th>
+								<th style="text-align:center;">Quantity</th>
+								<th style="text-align:center;">Unit Price</th>
+								<th style="text-align:right;">Item Total</th>
+							</tr>
+						</thead>
+						<tbody>';
+							//get discounts
+							$discounts = ['S' => [], 'P' => [], 'O' => []];
+							$sth = $dbh->prepare(
+								'SELECT name, type, amount, appliesToType, appliesToID
+								FROM discounts, orders_discounts
+								WHERE orderID = :orderID AND discounts.discountID = orders_discounts.discountID');
+							$sth->execute([':orderID' => $id]);
+							while ($row = $sth->fetch()) {
+								if ($row['appliesToType'] == 'O') {
+									$discounts['O'][] = [$row['name'], $row['type'], $row['amount']];
+								}
+								else {
+									$discounts[$row['appliesToType']][$row['appliesToID']][] = [$row['name'], $row['type'], $row['amount']];
+								}
+							}
+							
+							//get services
+							$sth = $dbh->prepare(
+								'SELECT services.serviceID, name, quantity, unitPrice
+								FROM services, orders_services
+								WHERE orderID = :orderID AND services.serviceID = orders_services.serviceID');
+							$sth->execute([':orderID' => $id]);
+							while ($row = $sth->fetch()) {
+								$return .= '<tr><td>'.$row['name'].'</td>';
+								$return .= '<td style="text-align:center;">'.($row['quantity'] + 0).'</td>'; //remove extra zeros and decimals
+								$return .= '<td style="text-align:center;">'.formatCurrency($row['unitPrice']).'</td>';
+								$lineAmount = $row['quantity'] * $row['unitPrice'];
+								$subTotal += $lineAmount;
+								$return .= '<td style="text-align:right;">'.formatCurrency($lineAmount).'</td></tr>';
+								
+								//apply any discounts to this item
+								if (count($discounts['S']) > 0) {
+									foreach ($discounts['S'][$row['serviceID']] as $discount) {
+										$return .= '<tr><td style="padding-left: 50px;">Discount: '.$discount[0].'</td><td></td><td></td>';
+										$discountAmount = ($discount[1] == 'P') ? $lineAmount * ($discount[2] / 100) : $row['quantity'] * $discount[2];
+										$subTotal -= $discountAmount;
+										$return .= '<td style="text-align:right;">-'.formatCurrency($discountAmount).'</td></tr>';
+									}
+								}
+							}
+							
+							//get products
+							$sth = $dbh->prepare(
+								'SELECT products.productID, name, quantity, unitPrice
+								FROM products, orders_products
+								WHERE orderID = :orderID AND products.productID = orders_products.productID');
+							$sth->execute([':orderID' => $id]);
+							while ($row = $sth->fetch()) {
+								$return .= '<tr><td>'.$row['name'].'</td>';
+								$return .= '<td style="text-align:center;">'.($row['quantity'] + 0).'</td>'; //remove extra zeros and decimals
+								$return .= '<td style="text-align:center;">'.formatCurrency($row['unitPrice']).'</td>';
+								$lineAmount = $row['quantity'] * $row['unitPrice'];
+								$subTotal += $lineAmount;
+								$return .= '<td style="text-align:right;">'.formatCurrency($lineAmount).'</td></tr>';
+								
+								//apply any discounts to this item
+								if (count($discounts['P']) > 0) {
+									foreach ($discounts['P'][$row['productID']] as $discount) {
+										$return .= '<tr><td style="padding-left: 50px;">Discount: '.$discount[0].'</td><td></td><td></td>';
+										$discountAmount = ($discount[1] == 'P') ? $lineAmount * ($discount[2] / 100) : $row['quantity'] * $discount[2];
+										$subTotal -= $discountAmount;
+										$return .= '<td style="text-align:right;">-'.formatCurrency($discountAmount).'</td></tr>';
+									}
+								}
+							}
+							
+							//apply order discounts
+							if (count($discounts['O']) > 0) {
+								foreach ($discounts['O'] as $discount) {
+									$return .= '<tr><td>Discount: '.$discount[0].'</td><td></td><td></td>';
+									$discountAmount = ($discount[1] == 'P') ? ($subTotal) * ($discount[2] / 100) : $discount[2];
+									$subTotal -= $discountAmount;
+									$return .= '<td style="text-align:right;">-'.formatCurrency($discountAmount).'</td></tr>';
+								}
+							}
+							$return .= '</tbody></table>';
+							
+							//find amount paid
+							$sth = $dbh->prepare(
+								'SELECT SUM(amount)
+								FROM payments
+								WHERE orderID = :orderID');
+							$sth->execute([':orderID' => $id]);
+							$row = $sth->fetch();
+							$paidAmount = $row['SUM(amount)'];
+							
+							//print totals
+							$return .= '<table style="width:100%; text-align:right;"><tbody>';
+							$return .= '<tr><td>Total:</td><td>'.formatCurrency($subTotal).'</td></tr>';
+							$return .= '<tr><td>Amount Paid:</td><td>'.formatCurrency($paidAmount).'</td></tr>';
+							$return .= '<tr style="font-weight: bold;"><td>Amount Due:</td><td>'.formatCurrency($subTotal - $paidAmount).'</td></tr>';
+						
+						$return .= '</tbody>
+					</table>
+				</div>
+			</section>';
 			
 			return $return;
 		}
