b9855072fc4a920ba7c9092b478721430200760b
Nicholas Anderson <nicholas.anderson2010@gmail.com>
2014-08-28 13:27:49 -0400

    Changed the fetch mode from assoc to the default.

--- init.php
+++ init.php
@@ -44,8 +44,7 @@
 		$SETTINGS['dbUser'],
 		$SETTINGS['dbPassword'],
 		[
-			PDO::ATTR_PERSISTENT => true,
-			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+			PDO::ATTR_PERSISTENT => true
 		]
 	);
 	
