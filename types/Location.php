da71d4fea4536f30bb22c414c33a7e6de761f03c
Nicholas Anderson <nicholas@audemium.com>
2015-03-19 15:30:47 -0400

    Change license from GPLv3 to AGPLv3.

--- types/Location.php
+++ types/Location.php
@@ -2,13 +2,13 @@
 	/*
 	Copyright (C) 2014 Nicholas Anderson
 	
-	This file is part of ERPxyz.  ERPxyz is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+	This file is part of ERPxyz.  ERPxyz is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by
     the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
     ERPxyz is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along with ERPxyz.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU Affero General Public License along with ERPxyz.  If not, see <http://www.gnu.org/licenses/>.
 	*/
 
 	class Location extends GenericItem {
