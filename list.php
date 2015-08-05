283b658cf11ccc041a9a9009787d60da980b9eb1
Nicholas Anderson <nicholas.anderson2010@gmail.com>
2015-08-04 20:23:08 -0400

    Commented out unused icon for now.

--- list.php
+++ list.php
@@ -125,9 +125,9 @@
 				<a class="controlEdit editDisabled" href="#" title="Select one or more rows to edit.">Edit</a>
 				<a class="controlDelete deleteDisabled" href="#" title="Select one or more rows to delete.">Delete</a>
 			</div>
-			<div id="topControlRight">
+			<!--<div id="topControlRight">
 				<a class="settings" href="#"></a>
-			</div>
+			</div>-->
 		</div>
 		<table id="itemTable" class="stripe row-border"> 
 			<thead>
