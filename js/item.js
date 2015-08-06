01ef6fffdf290b251b793a1e4aa0e12e7dc27a20
Nicholas Anderson <nicholas.anderson2010@gmail.com>
2015-08-06 13:50:50 -0400

    Break some no longer small js out into separate files.

---
+++ js/item.js
@@ -0,0 +1,185 @@
+$(document).ready(function() {
+	//set up dataTables
+	$('.dataTable').not('#historyTable').DataTable({
+		'paging': false,
+		'dom': 't',
+		'order': [0, 'desc'],
+		'autoWidth': false,
+		'columnDefs': [
+			{'width': '150px', 'targets': 'dateTimeHeader'}
+		]
+	});
+	
+	//set up datetimepicker
+	$('.timeInput').timepicker({
+		timeFormat: timeFormatJS,
+		stepMinute: 15
+	});
+	$('.dateInput').datepicker({
+		dateFormat: dateFormatJS
+	});
+	
+	//get initial history datatable, set up view all click
+	$.ajax({
+		url: 'ajax.php',
+		type: 'POST',
+		data: {
+			'action': 'history',
+			'type': type,
+			'id': id,
+			'limit': 5
+		}
+	}).done(function(data) {
+		$('#historyTable tbody').html(data.html);
+		$('#historyTable').DataTable({
+			'paging': false,
+			'dom': 't',
+			'order': [0, 'desc'],
+			'autoWidth': false,
+			'columnDefs': [
+				{'width': '150px', 'targets': 'dateTimeHeader'}
+			]
+		});
+	});
+	$('#historyTable .tableFooter a').click(function(event) {
+		$.ajax({
+			url: 'ajax.php',
+			type: 'POST',
+			data: {
+				'action': 'history',
+				'type': type,
+				'id': id,
+				'limit': -1
+			}
+		}).done(function(data) {
+			var temp = $('#historyTable').DataTable();
+			temp.destroy();
+			$('#historyTable tbody').html(data.html);
+			$('#historyTable').DataTable({
+				'paging': false,
+				'dom': 't',
+				'order': [0, 'desc'],
+				'autoWidth': false,
+				'columnDefs': [
+					{'width': '150px', 'targets': 'dateTimeHeader'}
+				]
+			});
+			$('#historyTable .tableFooter a').remove();
+		});
+		event.preventDefault();
+	});
+	
+	//qtip
+	$('#controls [title]').qtip({
+		'style': {'classes': 'qtip-tipsy-custom'},
+		'position': {
+			'my': 'bottom center',
+			'at': 'top center',
+			'adjust': {'y': -12}
+		}
+	});
+	if (active == 1) {
+		$('#topControlCenter .controlEdit').addClass('editEnabled').removeClass('editDisabled');
+		$('#topControlCenter .controlDelete').addClass('deleteEnabled').removeClass('deleteDisabled');
+		$('#topControlCenter .controlEdit, #topControlCenter .controlDelete').removeAttr('title');
+	}
+	
+	//add attachments
+	$('#addAttachment').click(function(event) {
+		$('#attachmentPopup').show();
+		$('#attachmentPopup input').val('');
+		//re-enable fields due to Firefox "feature"
+		$('#attachmentBtn').prop('disabled', false);
+		$('#attachmentPopup input').prop('disabled', false);
+		//need to remove errors and reset progress bar css in case this is after an upload error
+		$('#attachmentPopup .invalid').qtip('destroy', true);
+		$('#attachmentPopup .invalid').removeClass('invalid');
+		$('.meter > span').css('width', '0%');
+		$('.meter > span').css('border-radius', '16px 0 0 16px');
+		
+		$('#attachmentBtn').click(function() {
+			var formData = new FormData($('#attachmentPopup form').get(0));
+			formData.append('action', 'addAttachment');
+			formData.append('type', type);
+			formData.append('id', id);
+			$('#attachmentBtn').prop('disabled', true);
+			$('#attachmentPopup input').prop('disabled', true);
+			$('#attachmentBtn').addClass('buttonDisabled');
+
+			$.ajax({
+				url: 'ajax.php',
+				type: 'POST',
+				cache: false,
+				processData: false,
+				contentType: false,
+				data: formData,
+				xhr: function() {
+					myXhr = $.ajaxSettings.xhr();
+					if (myXhr.upload) {
+						myXhr.upload.addEventListener('progress', uploadProgress, false);
+					}
+					return myXhr;
+				}
+			}).done(function(data) {
+				$('#attachmentPopup .invalid').qtip('destroy', true);
+				$('#attachmentPopup .invalid').removeClass('invalid');
+				if (data.status == 'success') {
+					location.reload();
+				}
+				else {
+					$('#attachmentBtn').prop('disabled', false);
+					$('#attachmentPopup input').prop('disabled', false);
+					$('#attachmentBtn').removeClass('buttonDisabled');
+					$('#attachmentPopup input').addClass('invalid');
+					$('#attachmentPopup input').qtip({
+						'content': data.uploadFile,
+						'style': {'classes': 'qtip-tipsy-custom'},
+						'position': {
+							'my': 'bottom center',
+							'at': 'top center'
+						},
+						show: {
+							'event': false,
+							'ready': true
+						},
+						hide: {
+							'event': 'click',
+							'target': $('#attachmentPopup .close')
+						}
+					});
+				}
+			});
+		});
+		event.preventDefault();
+	});
+	
+	function uploadProgress(event) {
+		if (event.lengthComputable) {
+			var percent = Math.round((event.loaded / event.total) * 100);
+			$('.meter > span').css('width', percent + '%');
+			if (percent > 98) {
+				$('.meter > span').css('border-radius', '16px');
+			}
+		}
+	}
+	
+	//delete attachments
+	$('.attachmentTable .controlDelete').click(function(event) {
+		var $button = $(this);
+		if ($button.hasClass('deleteEnabled')) {
+			$.ajax({
+				url: 'ajax.php',
+				type: 'POST',
+				data: {
+					'action': 'deleteAttachment',
+					'type': type,
+					'id': id,
+					'subID':  $button.data('id')
+				}
+			}).done(function(data) {
+				location.reload();
+			});
+		}
+		event.preventDefault();
+	});
+});
--- list.php
+++ list.php
@@ -27,88 +27,8 @@
 		//echo date/time formats
 		var dateFormatJS = '<?php echo $SETTINGS['dateFormatJS'] ?>';
 		var timeFormatJS = '<?php echo $SETTINGS['timeFormatJS'] ?>';
-	
-		$(document).ready(function() {
-			//change topControls and header when hitting the top of the page
-			//TODO: fix column width changing when header hits the top
-			$('#topControls').data('top', $('#topControls').offset().top);
-			$(window).scroll(function() {
-				if ($(window).scrollTop() > $('#topControls').data('top')) { 
-					$('#topControls').css({
-						'position': 'fixed',
-						'top': '0',
-						'width': $('#content').width(),
-						'border-radius': '0 0 0 0'
-					});
-					$('thead').css({
-						'position': 'fixed',
-						'top': $('#topControls').css('height'),
-						'width': $('#content').width(),
-						'border-radius': '0 0 8px 8px'
-					}); 
-				}
-				else {
-					$('#topControls').css({
-						'position': 'static',
-						'top': 'auto',
-						'width': '100%',
-						'border-radius': '8px 8px 0 0'
-					});
-					$('thead').css({
-						'position': 'static',
-						'top': 'auto',
-						'width': '100%',
-						'border-radius': '0 0 0 0'
-					});
-				}
-			});
-
-			//set up dataTables
-			var table = $('#itemTable').DataTable({
-				'paging': false,
-				'dom': 'rti',
-				'order': [1, 'asc'],
-				'columnDefs': [
-					{'orderable': false, 'targets': 0},
-					{'searchable': false, 'targets': 0}
-				]
-			});
-			$('#filter').on('keyup', function() {
-				table.search(this.value).draw();
-			});
-			
-			//checkboxes
-			$('.selectCheckbox').click(function() {
-				checkCheckboxes();
-			});
-			
-			//qtip
-			$('#topControls [title]').qtip({
-				'style': {'classes': 'qtip-tipsy-custom'},
-				'position': {
-					'my': 'bottom center',
-					'at': 'top center',
-					'adjust': {'y': -12}
-				}
-			});
-			
-			//run in any case browser cached any checks
-			checkCheckboxes();
-		});
-		
-		function checkCheckboxes() {
-			if ($('.selectCheckbox:checked').length > 0) {
-				$('#topControlCenter .controlEdit').addClass('editEnabled').removeClass('editDisabled');
-				$('#topControlCenter .controlDelete').addClass('deleteEnabled').removeClass('deleteDisabled');
-				$('#topControlCenter .controlEdit, #topControlCenter .controlDelete').qtip('disable');
-			}
-			else {
-				$('#topControlCenter .controlEdit').addClass('editDisabled').removeClass('editEnabled');
-				$('#topControlCenter .controlDelete').addClass('deleteDisabled').removeClass('deleteEnabled');
-				$('#topControlCenter .controlEdit, #topControlCenter .controlDelete').qtip('enable');
-			}
-		}
 	</script>
+	<script type="text/javascript" src="js/list.js"></script>
 </head>
 
 <body>
