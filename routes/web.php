2141b865f657c7b1930a557a54af8597a769872e
nanderson11 <nanderson11@users.noreply.github.com>
2023-06-04 19:15:33 +0000

    Fix styling

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'dashboard');
 
 Route::get('/{type}', function (string $type) {
-    return view('list', ['type' => $type]);
+	return view('list', ['type' => $type]);
 });
