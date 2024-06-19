13c133888c8d1d0b057cbd85e9b536ae9a9e2b96
nanderson11 <nanderson11@users.noreply.github.com>
2024-06-19 21:41:59 +0000

    Fix styling

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Location extends Model {
 	public function employees(): HasMany {
